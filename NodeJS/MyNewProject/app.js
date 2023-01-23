7dbd1d3ddf8e095ed83f241623a6c1127a7b1b8e
Roman Smal <98179242+Monseratti@users.noreply.github.com>
2023-01-23 21:29:16 +0200

    commit

--- NodeJS/MyNewProject/app.js
+++ NodeJS/MyNewProject/app.js
@@ -1,14 +1,15 @@
 var http = require("http");
-http.createServer(function( request,response){response.end("Hello NodeJS");})
-.listen(3000, "127.0.0.1",function(){console.log("Сервер начал прослушивание запросов на порту 3000");});
-
-var ops = require("os");
-let usName = ops.userInfo().username;
-console.log(usName);
-
-var myfile = require("./app_two");
-console.log(myfile.MyHello(usName));
-
-var pers = require("./app_three");
-var man =  new pers(usName, "0977715630", 28);
-console.log(man.toString());
+var server = http.createServer(function(request,response){
+    var url = request.url.split('/')[1].toLowerCase();
+    switch(url){
+        case "home": response.end("Welcome home!");
+            break;
+        case "about":response.end("Welcome to about!");
+            break;
+        default:
+            response.statusCode = 404;
+            response.text = "Not Found";
+            response.end(response.text);
+    };
+});
+server.listen(3000, "127.0.0.1",function(){console.log("Сервер начал прослушивание запросов на порту 3000");});
